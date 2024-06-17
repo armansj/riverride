e7cf4b674cbbc8c4469ba81d1373335b4810c415
armansajedi944 <armansajedi944@gmail.com>
2024-06-17 05:10:07 -0700

    Add new features like remove screen and some changes for click events

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,99 @@
-use std::io::{stdout, Write};
+use std::{io::{stdout, Stdout, Write}, time::Duration};
 
 use crossterm::{
-    cursor::MoveTo,
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand,
-    event,
+    cursor::MoveTo, event::{self, poll, read, Event, KeyCode}, execute, style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor}, terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType}, ExecutableCommand, QueueableCommand
     
 };
 
+
+
+struct World{
+
+    player_C : u16,
+    player_l : u16,
+
+}
+
+fn draw(mut sc: &Stdout,world : &World) -> std::io::Result<()> {
+    sc.queue(Clear(ClearType::All))?;
+
+    sc.queue(MoveTo(world.player_C,world.player_l))?;
+    sc.queue(Print("Styled text here."))?;
+
+    sc.flush();
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
-    
+
 
     // or using functions
-    let mut sc = stdout();
+    let mut sc = stdout(); 
+    let (maxc,maxl)=size().unwrap();
+
+
+    enable_raw_mode()?;
+ 
+
+    // init the game
+    let mut world :World = World{
+        player_C : maxc /2 ,
+        player_l : maxl - 1,
+
+    };
+
+
+
+    loop {
+
+        if poll(Duration::from_millis(10))? {
+            // It's guaranteed that the `read()` won't block when the `poll()`
+            // function returns `true`
+            let key = read().unwrap();
+            match key {
+                Event::Key(event) => {
+                    match  event.code {
+
+                        KeyCode::Char('q')=>{
+                            break;
+                        }
+                        KeyCode::Char('w')=>{
+                        if(world.player_l > 1 ){
+                            world.player_l -=1
+                        };
+                        }
+                        KeyCode::Char('s')=>{
+                        if(world.player_l < maxl-1 ){
+                            world.player_l +=1
+                            };
+                            }
+                        KeyCode::Char('a')=>{
+                        if(world.player_C > 1 ){
+                            world.player_C -=1
+                                };
+                                }
+                        KeyCode::Char('d')=>{
+                        if(world.player_C < maxc-1 ){
+                            world.player_C +=1
+                             };
+                                    }
+                        _=> {},
+                        
+                    }
+                }, 
+                _=>{},     
+            }
+        } else {
+            // Timeout expired and no `Event` is available
+        }
+
+        // physics()
+
+
+        draw(&sc,&world)?;
+    }
     
-    sc.execute(MoveTo(6,4));
-    sc.execute(Print("Styled text here."))?;
-    
-    
+    disable_raw_mode();
     Ok(())
     
 }
